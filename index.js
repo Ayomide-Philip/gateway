509154ff741774e8796f857780dc3cbaf1efe4fe
A-pilip <areoayomide2008@gmail.com>
2024-10-14 23:15:18 +0100

    updating my server script

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  req.route.path == "/" ? console.log(true) : console.log(false);
+  res.render("index.ejs");
+});
+
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
