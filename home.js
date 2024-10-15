7f4b8774e007f3faa0e0321c8928a5aa28ec26d2
A-pilip <areoayomide2008@gmail.com>
2024-10-15 17:23:02 +0100

    updating the home.js

---
+++ home.js
@@ -0,0 +1,10 @@
+const welcome = {
+  heading: "Welcome to Gateway Baptist Church: Where Faith Meets Community",
+  paragraph: [
+    "At Gateway Baptist Church, we extend a heartfelt welcome to you. Our church is a place where faith and community converge, forming the cornerstone of our identity. Here, we believe in the transformative power of faith and the strength found in a supportive community. Whether you're exploring   your spirituality for the first time or seeking a welcoming community to grow in your faith, you've found a place to belong.",
+    "Join us as we worship together, study the Word, and engage in meaningful fellowship.  Experience the warmth of our community and discover how faith can enrich your life. At Gateway Baptist  Church, you'll find not just a place of worship, but a family ready to welcome you with open arms.",
+  ],
+  button: { name: "About Us", url: "/history" },
+};
+
+export default welcome;
