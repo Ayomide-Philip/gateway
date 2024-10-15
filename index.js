906519f2079cbf266d79f3bb6fa2ae2b27c88410
A-pilip <areoayomide2008@gmail.com>
2024-10-15 16:46:51 +0100

    updating index.js

--- index.js
+++ index.js
@@ -15,52 +15,52 @@ app.get("/", (req, res) => {
 
 app.get("/children", (req, res) => {
   req.route.path == "/children" ? (userRoute = "Children") : null;
-  res.render("children.ejs");
+  res.render("children.ejs", { userRoute: userRoute });
 });
 
 app.get("/contact", (req, res) => {
   req.route.path == "/contact" ? (userRoute = "Contact") : null;
-  res.render("contact.ejs");
+  res.render("contact.ejs", { userRoute: userRoute });
 });
 
 app.get("/donate", (req, res) => {
   req.route.path == "/donate" ? (userRoute = "Donate") : null;
-  res.render("donate.ejs");
+  res.render("donate.ejs", { userRoute: userRoute });
 });
 
 app.get("/events", (req, res) => {
   req.route.path == "/events" ? (userRoute = "Events") : null;
-  res.render("events.ejs");
+  res.render("events.ejs", { userRoute: userRoute });
 });
 
 app.get("/get-involved", (req, res) => {
   req.route.path == "/get-involved" ? (userRoute = "Get Involved") : null;
-  res.render("getInvolved.ejs");
+  res.render("getInvolved.ejs", { userRoute: userRoute });
 });
 
 app.get("/history", (req, res) => {
   req.route.path == "/history" ? (userRoute = "History") : null;
-  res.render("history.ejs");
+  res.render("history.ejs", { userRoute: userRoute });
 });
 
 app.get("/mission", (req, res) => {
   req.route.path == "/mission" ? (userRoute = "Mission") : null;
-  res.render("mission.ejs");
+  res.render("mission.ejs", { userRoute: userRoute });
 });
 
 app.get("/sermon", (req, res) => {
   req.route.path == "/sermon" ? (userRoute = "Sermon") : null;
-  res.render("sermon.ejs");
+  res.render("sermon.ejs", { userRoute: userRoute });
 });
 
 app.get("/service", (req, res) => {
   req.route.path == "/service" ? (userRoute = "Service") : null;
-  res.render("service.ejs");
+  res.render("service.ejs", { userRoute: userRoute });
 });
 
 app.get("/teenager", (req, res) => {
   req.route.path == "/teenager" ? (userRoute = "Teenager") : null;
-  res.render("teenager.ejs");
+  res.render("teenager.ejs", { userRoute: userRoute });
 });
 
 app.listen(port, () => {
