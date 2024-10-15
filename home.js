c919be12f13646d22b1b0af45776705ae660271c
A-pilip <areoayomide2008@gmail.com>
2024-10-15 22:57:14 +0100

    updating home.js

--- home.js
+++ home.js
@@ -25,6 +25,31 @@ const statementOfFaiths = {
       paragraph:
         "eternally existing in three persons: Father, Son, and Holy Spirit, each fully God yet distinct in role and function.We affirm the biblical teaching  that God is one essence existing in three distinct persons. The Father, Son (Jesus Christ), and Holy Spirit are co-eternal and co-equal, each playing a unique role in creation, redemption, and sustaining the universe.",
     },
+
+    {
+      id: "Two",
+      heading: "In the Authority of Scripture",
+      strongText:
+        "We believe the Bible is the inspired and authoritative Word of God,",
+      paragraph:
+        "  revealing God’s will, character, and plan for humanity.Scripture is the divinely inspired record  of  God's  revelation to humanity. It is authoritative in all matters of faith, doctrine, and  practice, providing guidance for living a life pleasing to God.",
+    },
+
+    {
+      id: "Three",
+      heading: "In Salvation through Jesus Christ",
+      strongText: "We believe in the deity of Jesus Christ,",
+      paragraph:
+        " His virgin birth, sinless life,  atoning death on the cross, bodily resurrection, ascension to heaven, and future return in power and glory. Jesus Christ, as fully God and fully human, lived a sinless life and sacrificed  Himself on the cross to atone for the sins of humanity. Through faith in Christ, we  receive   forgiveness of sins and eternal life.",
+    },
+
+    {
+      id: "Four",
+      heading: "In Justification by Faith",
+      strongText: "We believe that salvation is by grace alone",
+      paragraph:
+        ", through faith in Jesus Christ alone, and not by our own works or merit. Salvation is a gift of God's grace, received through faith in Jesus Christ. It is not earned through good works or human effort but is freely given  to all who believe in Christ as Savior and Lord. ",
+    },
   ],
 };
 
