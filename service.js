c83bc1af7b9ce49e95ebbdf8033a16427a62cc5e
A-pilip <areoayomide2008@gmail.com>
2024-10-16 15:40:23 +0100

    updating service.js

---
+++ service.js
@@ -0,0 +1,31 @@
+const service = {
+  topHeading: "Usual Church Activites",
+  sundayService: [
+    {
+      header: "Sunday Service",
+      title: "Teenager's Service",
+      text: "Our Teenagers Service offers engaging worship, relevant teachings, and fun  activities. It’s a space where teens can connect, grow in faith, and build lasting  friendships. Join us!",
+      footer: " 8:00am - 10:15 am",
+    },
+    {
+      header: "Sunday Service",
+      title: "English Service",
+      text: "Our English service offers a vibrant worship experience with inspiring  sermons, and a welcoming community. Join us every Sunday to connect, grow, and be spiritually enriched.",
+      footer: " 7:00am - 9:00am",
+    },
+    {
+      header: "Sunday Service",
+      title: "Sunday School",
+      text: "Sunday School nurtures Christian’s faith through engaging lessons, and fun activities. It’s a foundational time to learn about God’s love and build a strong spiritual  base.",
+      footer: "9:00am - 10:00am",
+    },
+    {
+      header: "Sunday Service",
+      title: "Yoruba Service",
+      text: "Our Yoruba service offers enriching worship with traditional hymns, powerful sermons, and a sense of community, all in the beautiful Yoruba language. Celebrate and grow in faith with us.",
+      footer: "10:00 am - 12:00 noon",
+    },
+  ],
+};
+
+export default service;
