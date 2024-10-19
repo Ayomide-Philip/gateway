66bbce5440a4b1019641f7f07e42249b63d6d7f1
A-pilip <areoayomide2008@gmail.com>
2024-10-19 22:53:02 +0100

    updating teenager.js

--- teenager.js
+++ teenager.js
@@ -2,9 +2,11 @@ const teenager = {
   topHeading: "Welcome to Our Teenager Church!",
   firstP:
     "At Harmony Community Church, we recognize the unique needs and challenges of teenagers as they navigate  this  pivotal stage of life. Our Teenager Church is dedicated to providing a supportive, engaging, and faith-building environment where teens can explore their faith, build meaningful relationships, and discover their God-given potential.",
-  h2: "Our Mission",
-  secondP:
-    "Our mission is to inspire and equip teenagers to grow in their relationship with Jesus Christ, develop strong moral character, and become leaders in their communities. We aim to create a space where teens feel  valued, heard, and empowered to make a positive impact in the world.",
+  mission: {
+    heading: "Our Mission",
+    missionText:
+      "Our mission is to inspire and equip teenagers to grow in their relationship with Jesus Christ, develop strong moral character, and become leaders in their communities. We aim to create a space where teens feel  valued, heard, and empowered to make a positive impact in the world.",
+  },
   offer: {
     topHeading: "What We Offer",
     list: [
