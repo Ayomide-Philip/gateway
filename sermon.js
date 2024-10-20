d6e33503774395b797019897252f838ceb4ddab2
A-pilip <areoayomide2008@gmail.com>
2024-10-20 11:50:37 +0100

    updating sermon.js

--- sermon.js
+++ sermon.js
@@ -7,6 +7,15 @@ const sermon = {
   ),
   videos: {
     heading: "Here are some of our Inspirational Sermons",
+    videoURL: [
+      "https://www.facebook.com/plugins/video.php?height=373&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F721412123398565%2F&show_text=false&width=560&t=0",
+      "https://www.facebook.com/plugins/video.php?height=373&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F856310806256114%2F&show_text=false&width=560&t=0",
+      "https://www.facebook.com/plugins/video.php?height=373&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F358968546896290%2F&show_text=false&width=560&t=0",
+      "https://www.facebook.com/plugins/video.php?height=373&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F1134273167625299%2F&show_text=false&width=560&t=0",
+      "https://www.facebook.com/plugins/video.php?height=373&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F7450460341732485%2F&show_text=false&width=560&t=0",
+      "https://www.facebook.com/plugins/video.php?height=373&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F7617311461692738%2F&show_text=false&width=560&t=0",
+      "https://www.facebook.com/plugins/video.php?height=373&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F300973816396437%2F&show_text=false&width=560&t=0",
+    ],
   },
 };
 
