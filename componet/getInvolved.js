2f8e81dd865215f3bf806ba08731cfb51c0532b0
A-pilip <areoayomide2008@gmail.com>
2024-10-20 22:17:01 +0100

    Creating getInvolved.js and adding some code to ti

---
+++ componet/getInvolved.js
@@ -0,0 +1,10 @@
+import createVideo from "./subcomponet/videoBanner.js";
+
+const getInvolved = {
+  videoBanner: createVideo(
+    "Get Involved at Gateway Baptist Church",
+    "Get involved in our church activities"
+  ),
+};
+
+export default getInvolved;
