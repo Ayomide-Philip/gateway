34a82f796fc8b97a8ed34ca2054f36006ab85864
A-pilip <areoayomide2008@gmail.com>
2024-10-20 11:10:00 +0100

    updating sermon.js

--- sermon.js
+++ sermon.js
@@ -1,1 +1,10 @@
 import createVideo from "./componet/videoBanner.js";
+
+const sermon = {
+  videoBanner: createVideo(
+    "Watch Our Sermons and Find Inspiration",
+    "Discover spiritual guidance through our sermons"
+  ),
+};
+
+export default sermon;
