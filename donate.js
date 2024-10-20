0d88302764d79ee1db7b2f476f9dcee60d21568c
A-pilip <areoayomide2008@gmail.com>
2024-10-20 12:32:57 +0100

    updating donate.js

--- donate.js
+++ donate.js
@@ -1,3 +1,10 @@
 import createVideo from "./componet/videoBanner.js";
 
-const donate = {};
+const donate = {
+  videoBanner: createVideo(
+    "Donate to Gateway Baptist Church",
+    "Your generous donations help Gateway Baptist Church fulfill its mission to serve our community"
+  ),
+};
+
+export default donate;
