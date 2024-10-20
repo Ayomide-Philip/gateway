3208cd186dc02522925800626dcda3a54d861cc6
A-pilip <areoayomide2008@gmail.com>
2024-10-20 15:57:18 +0100

    updating donate.js

--- donate.js
+++ donate.js
@@ -5,6 +5,30 @@ const donate = {
     "Donate to Gateway Baptist Church",
     "Your generous donations help Gateway Baptist Church fulfill its mission to serve our community"
   ),
+  heading: "Donate to Gateway Baptist Church",
+  p: "Your generous donations help Gateway Baptist Church fulfill its mission to serve our community, spread the Gospel, and support those in need. Every gift, no matter the size, makes a significant difference in advancing our work and vision. Thank you for partnering with us in this vital ministry.",
+  whyDonate: {
+    heading: "Why Your Donation Matters",
+    p: "At Gateway Baptist Church, we are committed to being a beacon of hope and love in our community and beyond. Your contributions enable us to",
+    list: [
+      {
+        heading: "Support Community Outreach",
+        text: "Provide meals, clothing, and essential services to those in need through our various outreach programs.",
+      },
+      {
+        heading: "Enhance Worship Services",
+        text: " Ensure that our worship services are vibrant, inclusive, and spiritually enriching for all attendees.",
+      },
+      {
+        heading: "Expand Educational Programs",
+        text: "Offer Bible studies, youth programs, and adult education classes that nurture spiritual growth and understanding.",
+      },
+      {
+        heading: "Fund Missionary Work",
+        text: "Support missionaries locally and globally, spreading the message of hope and salvation.",
+      },
+    ],
+  },
 };
 
 export default donate;
