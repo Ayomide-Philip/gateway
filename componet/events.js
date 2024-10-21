8137f01d3b440bb71951035819aee02967f4aaf2
A-pilip <areoayomide2008@gmail.com>
2024-10-21 12:42:27 +0100

    updating events.js

---
+++ componet/events.js
@@ -0,0 +1,10 @@
+import createVideo from "./subcomponet/videoBanner.js";
+
+const events = {
+  videoBanner: createVideo(
+    "Relive Our Memorable Church Events",
+    "Explore highlights from our past gatherings, like the Esther Picnic, and see how our community comes together."
+  ),
+};
+
+export default events;
