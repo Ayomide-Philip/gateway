52ba694f3113c83093404a2f42291c91208eaff6
A-pilip <areoayomide2008@gmail.com>
2024-10-21 22:13:57 +0100

    updating children.js

--- componet/children.js
+++ componet/children.js
@@ -1,5 +1,11 @@
 import createVideo from "./subcomponet/videoBanner.js";
 
-const children = {};
+const children = {
+  heading: "Meet Our Children’s Ministry Team",
+  videoBanner: createVideo(
+    "The Children Ministry",
+    "Learn more about the Children Ministry"
+  ),
+};
 
 export default children;
