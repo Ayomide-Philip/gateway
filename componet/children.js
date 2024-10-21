9710b31ae588a88ca84002adbea21646d4c9d3c7
A-pilip <areoayomide2008@gmail.com>
2024-10-21 22:19:34 +0100

    updating children.js

--- componet/children.js
+++ componet/children.js
@@ -8,6 +8,7 @@ const children = {
   ),
   leaders: {
     heading: "Our Leaders",
+    p: "At Gateway Baptist Church, our Children’s Ministry is blessed with a dedicated team of leaders who are passionate about nurturing the spiritual growth of our children. Meet the wonderful people who guide and inspire our kids each week",
   },
 };
 
