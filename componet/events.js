4c5ee54a014c297ae73c42b26027191c8f49740d
A-pilip <areoayomide2008@gmail.com>
2024-10-21 13:27:06 +0100

    updating events.js

--- componet/events.js
+++ componet/events.js
@@ -5,6 +5,44 @@ const events = {
     "Relive Our Memorable Church Events",
     "Explore highlights from our past gatherings, like the Esther Picnic, and see how our community comes together."
   ),
+  heading: "Celebrating Our Past Events",
+  videos: [
+    {
+      heading: "Here Are some of the Video From Our EASTER PICNIC 2024",
+      video: [
+        "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F614610444205679%2F&show_text=false&width=560&t=0",
+        "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F294916733498677%2F&show_text=false&width=560&t=0",
+        "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F436082135640598%2F&show_text=false&width=560&t=0",
+        "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F398181973004919%2F&show_text=false&width=560&t=0",
+        "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F1998822233878553%2F&show_text=false&width=560&t=0",
+        "",
+      ],
+    },
+    {
+      heading: "Here Are some of the Video From Our ROYAL AMBASSADOR WEEK 2024",
+      video: [
+        "https://www.facebook.com/plugins/video.php?height=373&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F496026033097865%2F&show_text=false&width=560&t=0",
+        "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F1962709577482922%2F&show_text=false&width=560&t=0",
+        "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F1622641895273435%2F&show_text=false&width=560&t=0",
+        "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F1395262367828535%2F&show_text=false&width=560&t=0",
+      ],
+    },
+    {
+      heading: "Here Are some of the Video From Our Children Day",
+      video: [
+        "https://www.facebook.com/plugins/video.php?height=373&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F789766829797271%2F&show_text=false&width=560&t=0",
+        "https://www.facebook.com/plugins/video.php?height=373&href=https%3A%2F%2Fweb.facebook.com%2Fgatewaybaptist.church.33%2Fvideos%2F468138169137353%2F&show_text=false&width=560&t=0",
+      ],
+    },
+    {
+      heading: "Here Are some of the Video From Our Father Day",
+      video: [],
+    },
+    {
+      heading: "Here Are some of the Video From Our MOTHER'S DAY SERVICE",
+      video: [],
+    },
+  ],
 };
 
 export default events;
