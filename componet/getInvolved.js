c98da5c7588b3c869ad953024e267bc95e82d858
A-pilip <areoayomide2008@gmail.com>
2024-10-21 10:23:45 +0100

    updating getInvolved.js

--- componet/getInvolved.js
+++ componet/getInvolved.js
@@ -7,6 +7,10 @@ const getInvolved = {
   ),
   heading: "Get Involved at Gateway Baptist Church",
   p: "At Gateway Baptist Church, we believe that everyone has a unique role to play in our community. Getting involved is a great way to grow in your faith, build relationships, and make a positive impact. Whether you have a few hours a month or a few hours a week, there are many ways you can get involved.",
+  whyInvolved: {
+    heading: "Why Get Involved?",
+    p: "Getting involved in church activities and ministries helps you to",
+  },
 };
 
 export default getInvolved;
