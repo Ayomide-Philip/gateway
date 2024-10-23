3602906ec969c801f95ee067db026dee57946611
A-pilip <areoayomide2008@gmail.com>
2024-10-23 21:42:12 +0100

    updating mission.js

--- componet/mission.js
+++ componet/mission.js
@@ -5,6 +5,10 @@ const mission = {
     "The Mission and Vision of the Church",
     "Learn more about the vision and mission of our Church"
   ),
+  greatCommission: {
+    heaading:
+      "The Gateway Baptist Church (Community of Grace) exists to fulfill the Great Commandment and the great Commission.",
+  },
 };
 
 export default mission;
