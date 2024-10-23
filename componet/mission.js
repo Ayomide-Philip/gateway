bce77d46aae447e9f4ae66bc01bf7aefb821a474
A-pilip <areoayomide2008@gmail.com>
2024-10-23 09:47:43 +0100

    updating mission.js

--- componet/mission.js
+++ componet/mission.js
@@ -1,1 +1,10 @@
 import createVideo from "./subcomponet/videoBanner.js";
+
+const mission = {
+  videoBanner: createVideo(
+    "The Mission and Vision of the Church",
+    "Learn more about the vision and mission of our Church"
+  ),
+};
+
+export default mission;
