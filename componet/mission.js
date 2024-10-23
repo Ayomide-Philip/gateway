3e20ac677ecb7abe40979ed126d519eae4e116e1
A-pilip <areoayomide2008@gmail.com>
2024-10-23 21:46:37 +0100

    updating mission.js

--- componet/mission.js
+++ componet/mission.js
@@ -1,12 +1,13 @@
 import createVideo from "./subcomponet/videoBanner.js";
 
 const mission = {
+  heading: "Our Purpose Statement | Our Mission and Vision as a Church.",
   videoBanner: createVideo(
     "The Mission and Vision of the Church",
     "Learn more about the vision and mission of our Church"
   ),
   greatCommission: {
-    heaading:
+    heading:
       "The Gateway Baptist Church (Community of Grace) exists to fulfill the Great Commandment and the great Commission.",
   },
 };
