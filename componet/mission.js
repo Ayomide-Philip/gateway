a16efac3eb2083fa9d36fbd1b921b6516ef52a7c
A-pilip <areoayomide2008@gmail.com>
2024-10-23 21:51:45 +0100

    updating mission.js

--- componet/mission.js
+++ componet/mission.js
@@ -9,6 +9,18 @@ const mission = {
   greatCommission: {
     heading:
       "The Gateway Baptist Church (Community of Grace) exists to fulfill the Great Commandment and the great Commission.",
+    figure: [
+      {
+        blockQuote:
+          "Love the Lord your God with all your heart and with your soul and with all your mind... Love your neighbour as yourself. All the law Prophets hang on these two commandement...",
+        blockQuoteFooter: "Matthew 22:37-40",
+      },
+      {
+        blockQuote:
+          "Go and make disciples of all nation baptizing them in the name of the Father and the Son and the Holy Spirit, and teaching them to obey everything i have commanded you.",
+        blockQuoteFooter: "Matthew 28:9-20",
+      },
+    ],
   },
 };
 
