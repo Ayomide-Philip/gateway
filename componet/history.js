ee037ff6a7f3a9a4279dc76e15895ef95cfea86c
A-pilip <areoayomide2008@gmail.com>
2024-10-24 21:58:12 +0100

    updating history.js

--- componet/history.js
+++ componet/history.js
@@ -31,6 +31,7 @@ const history = {
     heading: `Why the name was "Gateway" Baptist Church?`,
     p: `Have you ever wondered why the church was named "Gateway"? As at 1960, there was only an entry point from the northern part of the country. At the site are of the church was the airport. Also, it was and still is the only south-north route for the Nigeria Railway. At that time, hardly could anybody come to visit the Western Region without passing through the Sanngo area of the city. The worshippers, through the divine inspiration as voiced by Deacon Lawore, then leader of the preaching station, then considered "GATEWAY" BAPTIST CHURCH as the name because it was, and still is the Gateway to Heaven, Paradise, Saviour and salvation as well as, break-through, success, miracles, blessings, redemption and long life and prosperity.`,
   },
+  Pioneer
 };
 
 export default history;
