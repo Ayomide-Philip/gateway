1a915cf227514027f4c719cd0ef7a63e043bc14b
A-pilip <areoayomide2008@gmail.com>
2024-10-24 21:27:39 +0100

    updating history.js

--- componet/history.js
+++ componet/history.js
@@ -5,6 +5,12 @@ const history = {
     "The Church In the Historical Perspective.",
     "Learn more about our church"
   ),
+  heading: "Founding and Establishing the Church.",
+  background: {
+    heading: "Background Initiatives and Steps",
+    firstP:
+      "In Obedience to God Almighty Father's directive that all Christians should go into the world and preach the gospel, a preaching station of the Union Baptist Church, Ekotedo, in Ibadan city, began in October 1960. The vision to start church planting started with the Late Revd. Theophilus Adedeji Adejumobi became the pastor of the church. In one of his sermons on soul winning, in 1958, many people responded to become soul winners and the RIEL club [Religion In Everyday Life] was inaugurated . Some members of the club included Mr. Oladipo, Mr. A.O Fakorede(now Deacon Fakorede, Former president of Deacons fellowship of the Convection), Mr. A.B Farinde, Miss Bimpe Atanda(now Mrs. Adigun), Mr. Johnson Ogundele, Mr. Afuwape, Mr. D.O Amoo(Now Deacon) and late Dn. S.O Lawore. Members of this club became evangelism team of the church.",
+  },
 };
 
 export default history;
