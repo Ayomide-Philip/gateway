ec539f54da9bd2215d8518e698d4d642aa65c92f
A-pilip <areoayomide2008@gmail.com>
2024-10-24 20:31:03 +0100

    updating history.js

--- componet/history.js
+++ componet/history.js
@@ -1,5 +1,8 @@
 import createVideo from "./subcomponet/videoBanner.js";
 
 const history = {
-    
-}
+  videoBanner: createVideo(
+    "The Church In the Historical Perspective.",
+    "Learn more about our church"
+  ),
+};
